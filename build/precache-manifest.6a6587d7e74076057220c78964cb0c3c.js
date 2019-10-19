5cea0c1ee3d8334a46b6e130025a622fd024eb7a
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-10-20 00:21:08 +0300

    ready for deploy

---
+++ build/precache-manifest.6a6587d7e74076057220c78964cb0c3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6f21ce3450e8b111daf920696075f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84250e6e552c4f4ec839",
+    "url": "/static/js/2.cbf5d0b4.chunk.js"
+  },
+  {
+    "revision": "9bdf21e275c8a1b757f8",
+    "url": "/static/js/main.74b9d799.chunk.js"
+  },
+  {
+    "revision": "0a60ccf74c4fd02454bf",
+    "url": "/static/js/runtime-main.8f84b47d.js"
+  }
+]);
