c2fabc0c00f489b0970fcc818a618fd4cec402a6
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-10-19 21:49:02 +0300

    tehtavat 3.1-3.6

---
+++ index.js
@@ -0,0 +1,69 @@
+const data = {
+  "persons": [
+    {
+      "name": "Dan Abramov",
+      "number": "456-123123",
+      "id": 1
+    },
+    {
+      "name": "Alan Turing",
+      "number": "123-556677",
+      "id": 2
+    },
+    {
+      "name": "John von Neumann",
+      "number": "123-990077",
+      "id": 3
+    }
+  ]
+}
+
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+
+app.use(bodyparser.json())
+
+app.get('/api/persons', (req, res) => {
+    res.json(data)
+})
+app.get('/info', (req, res) => {
+    const info = `<p>Phonebook has ${data.persons.length} people</br>${new Date(Date.now()).toUTCString()}</p>`
+    res.send(info)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = data.persons.find(person => person.id === id)
+    if(person === undefined) res.status(404).end()
+    else res.json(person)
+})
+
+app.post('/api/persons/', (req, res) => {
+    let person = req.body
+    if(data.persons.find(nextPerson => nextPerson.name === person.name)) {
+        res.status(400).send({error: 'name must be unique'}).end()
+    }
+
+    if(!person.name || !person.number) {
+        res.status(400).send({error: 'name or number missing'}).end()
+    }
+
+    person = {
+        ...person,
+        id: Math.ceil(Math.random() * 10000)
+    }
+    data.persons = data.persons.concat(person)
+    res.status(204).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    data.persons = data.persons.filter(person => person.id !== id)
+    res.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`)
+})
