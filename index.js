2e0b4242ad789aa7e4a568da75cc97d26254ed48
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2019-10-20 19:42:22 +0300

    tehtava 3.22

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -12,71 +13,71 @@ app.use(bodyparser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', (req, res) => {
-  return JSON.stringify(req.body)
+    return JSON.stringify(req.body)
 })
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error.message)
-  return res.status(400).json({ error: error.message })
+    console.log(error.message)
+    return res.status(400).json({ error: error.message })
 }
 
 app.get('/api/persons', (req, res, next) => {
-  console.log('Getting persons from mongdodb')
-  Person.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()))
-    console.log('Persons returned succesfully')
-  }).catch(err => next(err))
+    console.log('Getting persons from mongdodb')
+    Person.find({}).then(persons => {
+        res.json(persons.map(person => person.toJSON()))
+        console.log('Persons returned succesfully')
+    }).catch(err => next(err))
 })
 app.get('/info', (req, res) => {
-  const info = `<p>Phonebook has ${data.persons.length} people</br>${new Date(Date.now()).toUTCString()}</p>`
-  res.send(info)
+    const info = `<p>Phonebook has ${data.persons.length} people</br>${new Date(Date.now()).toUTCString()}</p>`
+    res.send(info)
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-  const id = req.params.id
-  Person.findById(id).then(person => res.json(person))
-    .catch(error => next(error))
+    const id = req.params.id
+    Person.findById(id).then(person => res.json(person))
+        .catch(error => next(error))
 })
 
 app.post('/api/persons/', (req, res, next) => {
-  const body = req.body
+    const body = req.body
 
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-  person.save().then(savedPerson => {
-    res.json(savedPerson.toJSON()).status(204)
-  })
-    .catch(err => next(err))
+    person.save().then(savedPerson => {
+        res.json(savedPerson.toJSON()).status(204)
+    })
+        .catch(err => next(err))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(deletedPerson => {
-      res.json(deletedPerson.toJSON()).status(204)
-    })
-    .catch(err => next(err))
+    Person.findByIdAndRemove(req.params.id)
+        .then(deletedPerson => {
+            res.json(deletedPerson.toJSON()).status(204)
+        })
+        .catch(err => next(err))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
-  const person = {
-    name: body.name,
-    number: body.number
-  }
+    const body = req.body
+    const person = {
+        name: body.name,
+        number: body.number
+    }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then(updatedPerson => {
-      res.json(updatedPerson.toJSON())
-    })
-    .catch(err => next(err))
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson.toJSON())
+        })
+        .catch(err => next(err))
 })
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`)
+    console.log(`Listening to port ${PORT}`)
 })
